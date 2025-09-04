b05f7b8242a7e4cfd544c645da4bb5526754ab2d
Elias Rosenberg <122997687+EliasRosenberg-TomTom@users.noreply.github.com>
2025-09-04 13:51:34 -0400

    reading from correct instructions

--- agent_conf/AgentConf.py
+++ agent_conf/AgentConf.py
@@ -1,7 +1,7 @@
 class AgentConf:
 
     def load_agent_instructions(filename: str) -> str:
-        basePath = "instructions\\"
-        instructions_path = f"{basePath}{filename}"
+        basePath = "agent_instructions"
+        instructions_path = f"{basePath}\\{filename}"
         with open(instructions_path, "r", encoding="utf-8") as file:
             return file.read()
