d2aeca3cbdb44850d257390ce6f035a3e036a713
Elias Rosenberg <elias.rosenberg@tomtom.com>
2025-09-17 17:01:59 -0400

    making file path concat os agnostic

--- agent_conf/AgentConf.py
+++ agent_conf/AgentConf.py
@@ -1,7 +1,9 @@
+import os
+
 class AgentConf:
 
-    def load_agent_instructions(filename: str) -> str:
+    def load_agent_instructions(self, filename: str) -> str:
         basePath = "agent_instructions"
-        instructions_path = f"{basePath}\\{filename}"
+        instructions_path = os.path.join(basePath, filename)
         with open(instructions_path, "r", encoding="utf-8") as file:
             return file.read()
